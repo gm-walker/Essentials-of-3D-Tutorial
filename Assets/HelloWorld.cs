24a93e1781368ea2e7d48507dddeaa477df94243
@gm-walker <gm.walker2022@gmail.com>
2023-02-27 14:53:49 -0500

    Completed Unity Tutorial: Code in the default script

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string myMessage;
+    void Start()
+    {
+        Debug.Log(myMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
